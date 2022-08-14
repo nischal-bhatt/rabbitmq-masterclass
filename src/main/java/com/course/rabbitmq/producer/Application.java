3ed432fde0b214399343501852069c3eb6fdf12a
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-14 11:45:33 +0800

    fanout exchange

--- src/main/java/com/course/rabbitmq/producer/Application.java
+++ src/main/java/com/course/rabbitmq/producer/Application.java
@@ -8,14 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.course.rabbitmq.producer.entity.Employee;
-import com.course.rabbitmq.producer.producer.EmployeeJsonProducer;
+import com.course.rabbitmq.producer.producer.HumanResourceProducer;
 
 @SpringBootApplication
 //@EnableScheduling
 public class Application implements CommandLineRunner{
 
+	//@Autowired
+	//private EmployeeJsonProducer employeeJsonProducer;
+	
 	@Autowired
-	private EmployeeJsonProducer employeeJsonProducer;
+	private HumanResourceProducer humanResourceProducer;
 	
 	//@Autowired
 	//private HelloRabbitProducer producer;
@@ -28,10 +31,16 @@ public class Application implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 	//	this.producer.sendHello("Timotius Pamungkas" + Math.random());
 		
-		for (int i =0; i<5 ; i ++)
+	//	for (int i =0; i<5 ; i ++)
+	//	{
+	//		var employee = new Employee("emp" + i, "Employee " + i, LocalDate.now());
+	//	     employeeJsonProducer.sendMessage(employee);
+	//	}
+		
+		for (int i =0; i<5; i++)
 		{
-			var employee = new Employee("emp" + i, "Employee " + i, LocalDate.now());
-		     employeeJsonProducer.sendMessage(employee);
+			var employee = new Employee("emp"+i,"Employee" + i,LocalDate.now());
+			humanResourceProducer.sendMessage(employee);
 		}
 		
 	}
---
+++ src/main/java/com/course/rabbitmq/producer/producer/HumanResourceProducer.java
@@ -0,0 +1,28 @@
+package com.course.rabbitmq.producer.producer;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.course.rabbitmq.producer.entity.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class HumanResourceProducer {
+
+	
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	public void sendMessage(Employee employee) throws JsonProcessingException
+	{
+		var json = objectMapper.writeValueAsString(employee);
+		
+		rabbitTemplate.convertAndSend("x.hr","",json);
+		//first param is exchange name
+	}
+}
