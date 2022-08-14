50c209861fb847d82889f37155edca5c27dd8ad7
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-14 09:30:00 +0800

    asd

--- src/main/java/com/course/rabbitmq/consumer/consumer/HelloRabbitConsumer.java
+++ src/main/java/com/course/rabbitmq/consumer/consumer/HelloRabbitConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HelloRabbitConsumer {
-
+//typing
 	@RabbitListener(queues = "course.hello")
 	public void listen(String message)
 	{
