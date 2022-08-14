fc2cd244ea2339c23f983433aab9d798992635e5
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-08-14 11:44:48 +0800

    fanout exchange

--- src/main/java/com/course/rabbitmq/consumer/consumer/FixedRateConsumer.java
+++ src/main/java/com/course/rabbitmq/consumer/consumer/FixedRateConsumer.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class FixedRateConsumer {
 
 	
